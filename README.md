bb780065021bc4e66cf30cad516ed4109584d6f9
Andres Colonia <andreskal@gmail.com>
2016-06-23 17:49:00 -0400

    hello world :rocket:

---
+++ README.md
@@ -0,0 +1,7 @@
+#Parse-compatible API server module for Node/Express
+
+### Install Essential Packages
+`sudo npm install`
+
+### Run Server
+`npm start`
---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+var yaml = require('js-yaml');
+var fs = require("fs");
+var e = yaml.load(fs.readFileSync(__dirname + "/config/app.yaml"));
+
+console.log(e.DATABASE_URI);
+
+var databaseUri = e.DATABASE_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+  databaseURI: e.DATABASE_URI,
+  appId: e.APP_ID,
+  masterKey: e.MASTER_KEY,
+  serverURL: e.SERVER_URL,
+  verbose: true,
+  push: {
+    android: {
+      senderId: e.ANDROID_SENDER_ID,
+      apiKey: e.ANDROID_API_KEY
+    },
+  }
+});
+var app = express();
+
+var mountPath = e.PARSE_MOUNT;
+app.use(mountPath, api);
+
+var port = e.PORT;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
